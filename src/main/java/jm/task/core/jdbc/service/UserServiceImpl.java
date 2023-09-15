7909a93d6e08512c16c13932bc29dbaa17d9f1cb
Kseniia <103669056+MissKseniia@users.noreply.github.com>
2023-09-15 20:53:40 +0400

    JDBC realization

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,15 +2,14 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private static UserDao userDao = new UserDaoHibernateImpl();
-    //new UserDaoJDBCImpl();
+    private static UserDao userDao = new UserDaoJDBCImpl();
+
 
     @Override
     public void createUsersTable() {
