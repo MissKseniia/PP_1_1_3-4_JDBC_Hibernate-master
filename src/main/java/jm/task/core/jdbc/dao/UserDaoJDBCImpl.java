c47ae28f9a62a6cac7d9fd5eaf70b962ca1d9d8d
Kseniia <103669056+MissKseniia@users.noreply.github.com>
2023-09-18 17:03:23 +0400

    Update UserDaoJDBCImpl.java

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -27,7 +27,7 @@ public class UserDaoJDBCImpl implements UserDao {
         try (Statement statement = connection.createStatement()) {
 
             String query = "create table if not exists " + tableName +
-                    " (user_id int primary key auto_increment, " +
+                    " (user_id bigint primary key auto_increment, " +
                     "user_name varchar (10), " + "last_name varchar (15), " +
                     "user_age tinyint)";
             statement.execute(query);
@@ -55,9 +55,9 @@ public class UserDaoJDBCImpl implements UserDao {
         String sql = "insert into " + tableName + " (user_name, last_name, user_age) values (?, ?, ?) ";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
 
-            statement.setString (1, name);
-            statement.setString (2, lastName);
-            statement.setInt (3, age);
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -71,7 +71,7 @@ public class UserDaoJDBCImpl implements UserDao {
         try (PreparedStatement statement = connection.prepareStatement(
                 "delete from " + tableName + " where user_id = ?")) {
 
-            statement.setInt(1, Math.toIntExact(id));
+            statement.setLong(1, Math.toIntExact(id));
             statement.executeUpdate();
         } catch (SQLException | ArithmeticException e) {
             e.printStackTrace();
